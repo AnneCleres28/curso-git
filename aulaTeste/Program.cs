99669e404ee4b35145af51234926ca080a86ca0e
Anne Cleres <annecleres.ac@gmail.com>
2021-09-09 11:46:23 -0300

    Acrescentando tracos

--- aulaTeste/Program.cs
+++ aulaTeste/Program.cs
@@ -6,9 +6,13 @@ namespace aulaTeste
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("----------------------------------");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Como vovê está hoje?");
             Console.WriteLine("Tenha um bom dia!");
+            Console.WriteLine("----------------------------------");
+
+
         }
     }
 }
